a03c815d8454b04a1b433d82a7f3da9b1580b73e
PoyoMain <CLC9512@GMAIL.COM>
2024-07-07 18:51:14 -0400

    Inputs and Gameplay states added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,9 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
+    private GameState _state;
+
+    // 
 
     // Start is called before the first frame update
     void Start()
@@ -18,4 +21,66 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    private void ChangeState(GameState newState)
+    {
+        _state = newState;
+
+        switch (newState)
+        {
+            case GameState.Menu:
+                break;
+            case GameState.Scan:
+                StartCoroutine(ScanCoroutine());
+                break;
+            case GameState.Cast:
+                StartCoroutine(CastCoroutine());
+                break;
+            case GameState.Tuggle:
+                StartCoroutine(TuggleCoroutine());
+                break;
+            case GameState.Reel:
+                StartCoroutine(ReelCoroutine());
+                break;
+            case GameState.Results:
+                break;
+        }
+    }
+
+    #region Gameplay Methods
+
+    private IEnumerator ScanCoroutine()
+    {
+        ChangeState(GameState.Cast);
+        yield break;
+    }
+
+    private IEnumerator CastCoroutine()
+    {
+        ChangeState(GameState.Tuggle);
+        yield break;
+    }
+
+    private IEnumerator TuggleCoroutine()
+    {
+        ChangeState(GameState.Reel);
+        yield break;
+    }
+
+    private IEnumerator ReelCoroutine()
+    {
+        yield break;
+    }
+
+    #endregion
+}
+
+public enum GameState
+{
+    Menu,
+    Scan,
+    Cast,
+    Tuggle,
+    Reel,
+    Results
 }
