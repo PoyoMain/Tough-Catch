2f2a88d21c46ccec578da817de1823c6481e2791
PoyoMain <CLC9512@GMAIL.COM>
2024-07-11 20:43:25 -0400

    Adjusted Camera Position

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,16 +14,37 @@ public class GameManager : MonoBehaviour
     [SerializeField] private CinemachineVirtualCamera dockCam;
     [SerializeField] private CinemachineVirtualCamera lakeCam;
 
-    // Start is called before the first frame update
-    void Start()
+    private bool temp = false;
+
+    private void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Destroy(this);
+            Debug.LogError("More than one instance of GameManager in scene." +
+                "\n Located on " + this.name + " and " + Instance.name);
+        }
+
+        Instance = this;
+
+        VariableCheck();
     }
 
-    // Update is called once per frame
-    void Update()
+    void VariableCheck()
     {
-        
+        if (povCam == null) Debug.LogError("Pov Camera not set in Inspector");
+        if (dockCam == null) Debug.LogError("Dock Camera not set in Inspector");
+        if (lakeCam == null) Debug.LogError("Lake Camera not set in Inspector");
+    }
+
+    void Start()
+    {
+        ChangeState(GameState.Scan);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O)) temp = true;
     }
 
     private void ChangeState(GameState newState)
@@ -55,28 +76,61 @@ public class GameManager : MonoBehaviour
 
     private IEnumerator ScanCoroutine()
     {
+        ActivateCamera(lakeCam);
+
+        while (!temp)
+        {
+            yield return null;
+        }
+        temp = false;
+
         ChangeState(GameState.Cast);
         yield break;
     }
 
     private IEnumerator CastCoroutine()
     {
+        ActivateCamera(dockCam);
+
+        while (!temp)
+        {
+            yield return null;
+        }
+        temp = false;
+
         ChangeState(GameState.Tuggle);
         yield break;
     }
 
     private IEnumerator TuggleCoroutine()
     {
+        ActivateCamera(povCam);
+
+        while (!temp)
+        {
+            yield return null;
+        }
+        temp = false;
+
         ChangeState(GameState.Reel);
         yield break;
     }
 
     private IEnumerator ReelCoroutine()
     {
+        ActivateCamera(dockCam);
+
         yield break;
     }
 
     #endregion
+
+    private void ActivateCamera(CinemachineVirtualCamera newCam)
+    {
+        povCam.enabled = (povCam == newCam);
+        dockCam.enabled = (dockCam == newCam);
+        lakeCam.enabled = (lakeCam == newCam);
+    }
 }
 
 public enum GameState
