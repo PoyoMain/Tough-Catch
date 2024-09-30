2e2165e66c553adc277c896c070ffba33012a6b2
cjoyner150 <cjoyner150@gmail.com>
2024-09-30 14:00:15 -0400

    Most of the scanner minigame
    -movement
    -timers done
    -variables serialized

--- Assets/Scripts/Minigames/ScanMinigameManager.cs
+++ Assets/Scripts/Minigames/ScanMinigameManager.cs
@@ -1,20 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScanMinigameManager : MinigameBase
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Catch Time")]
+    [SerializeField] float minTime;
+    [SerializeField] float maxTime;
+
+    [Header("Fish Time")]
+    [SerializeField] float maxFishTime;
+
+    [Header("Dynamic")]
+    [SerializeField] float timeUntilCatch;
+    [SerializeField] float fishTimer;
+    [SerializeField] bool found;
+
+    ScannerMove scanner;
+    TextMeshProUGUI tmp;
+
+
+    private void Start()
     {
-        
+        scanner = ScannerMove.GET_SCANNER();
+        tmp = scanner.gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        tmp.text = "";
+        SetCatchTime();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //Run the timer while the scanner is moving and the fish is not found
+        if (!found && scanner.moving)
+        {
+            timeUntilCatch -= Time.deltaTime;
+        }
+
+        //Timer can only go off if fish is not found
+        if (!found && timeUntilCatch < 0)
+        {
+            //Stop the movement and update the exclamation text
+            scanner.Pause();
+            print("times up");
+            tmp.text = "!";
+
+            //Player found the fish
+            Find();
+        }
+
+        //While the fish is found 
+        if (found && !(fishTimer < 0))
+        {
+            if (Controls.Confirm.IsPressed() || Input.GetKeyDown(KeyCode.Space))
+            {
+                //Caught the fish
+                Catch();
+            }
+
+            fishTimer -= Time.deltaTime;
+
+            if (fishTimer < 0)
+            {
+                //Fish leaves
+                fishFlee();
+            }
+        }
     }
 
-    
+    void SetCatchTime()
+    {
+        timeUntilCatch = Random.Range(minTime, maxTime);
+    }
+
+    void Find()
+    {
+        found = true;
+        fishTimer = maxFishTime;
+    }
+
+    //Resets the minigame after the player fails to confirm
+    void fishFlee()
+    {
+        tmp.text = "";
+        scanner.Unpause();
+        SetCatchTime();
+        found = false;
+    }
+
+    void Catch()
+    {
+        print("Caught the fish!");
+    }
+
+
 }
---
+++ Assets/Scripts/ScannerMove.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScannerMove : MonoBehaviour
+{
+    Rigidbody rb;
+    float horizontalInput;
+    float verticalInput;
+
+    [SerializeField] float speed;
+    [Header("Bounds")]
+    [SerializeField] float xMax;
+    [SerializeField] float xMin;
+    [SerializeField] float yMax;
+    [SerializeField] float yMin;
+
+    public bool moving;
+    bool paused;
+
+    Vector3 move;
+
+    private static ScannerMove instance;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        instance = this;
+
+        paused = false;
+    }
+
+    
+    void Update()
+    {
+        if (paused) return;
+
+        //Handle Input
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        //Update moving bool when there is input
+        if (Mathf.Abs(verticalInput) > 0 || Mathf.Abs(horizontalInput) > 0)
+        {
+            moving = true;
+        }
+        else moving = false;
+
+        //Check bounds
+        Vector3 pos = transform.position;
+
+        if (pos.y > yMax) pos.y = yMax;
+        if (pos.y < yMin) pos.y = yMin;
+        if (pos.x > xMax) pos.x = xMax;
+        if (pos.x < xMin) pos.x = xMin;
+
+        transform.position = pos;
+    }
+
+    //Physics handled in fixed update
+    private void FixedUpdate()
+    {
+        if (paused) return;
+
+        move = Vector3.zero;
+
+        //Create vector from input
+        move.x = horizontalInput;
+        move.y = verticalInput;
+
+        //Normalize the vector to get direction and multiply by speed to get velocity
+        rb.velocity = move.normalized * speed;
+    }
+
+
+    /// <summary>
+    /// Pauses the movement of the scanner
+    /// </summary>
+    public void Pause()
+    {
+        rb.velocity = Vector3.zero;
+        paused = true;
+    }
+
+    /// <summary>
+    /// Unpauses the movement of the scanner
+    /// </summary>
+    public void Unpause()
+    {
+        paused = false;
+    }
+
+    /// <summary>
+    /// Public static method to retrieve the scanner singleton
+    /// </summary>
+    /// <returns></returns>
+    public static ScannerMove GET_SCANNER()
+    {
+        return instance;
+    }
+}
