89ba6cff2f4c1650f35aea0740c4dbfd568ca6ec
PoyoMain <CLC9512@GMAIL.COM>
2024-10-06 15:40:27 -0400

    Updated Build Settings

--- Assets/Scripts/Minigames/ReelMinigame.cs
+++ Assets/Scripts/Minigames/ReelMinigame.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.UI;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class ReelMinigame : MinigameBase
     
