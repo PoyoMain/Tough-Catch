ac7cd5ec63224468ea1b186365c0a5fb7bdb3010
PoyoMain <CLC9512@GMAIL.COM>
2024-11-01 10:37:13 -0400

    Redoing Cast Minigame

--- Assets/Scripts/Minigames/CastMinigame.cs
+++ Assets/Scripts/Minigames/CastMinigame.cs
@@ -52,6 +52,7 @@ public class CastMinigame : MinigameBase
     }
 
     private bool active = false;
+    private bool coroutinePlaying = false;
 
     protected override void OnEnable()
     {
@@ -124,8 +125,9 @@ public class CastMinigame : MinigameBase
     void Update()
     {
         if (!active) return;
+        if (coroutinePlaying) return;
 
-        if (targetRing != null && targetRing.transform.localScale.x < maxScale && MinigameDone == false)
+        if (targetRing != null && targetRing.transform.localScale.x < maxScale)
         {
             // Increase the scale over time at the specified rate
             float newScale = targetRing.transform.localScale.x + ScaleRate * Time.deltaTime;
@@ -134,45 +136,43 @@ public class CastMinigame : MinigameBase
             // Apply the new scale uniformly in all directions
             targetRing.transform.localScale = new Vector3(newScale, newScale, newScale);
 
-            if (ring1.transform.localScale.x < TargetRingScale.x && TargetRingScale.x < ring2.transform.localScale.x) buttonPromptImage.enabled = true;
-            else if (ring3.transform.localScale.x < TargetRingScale.x && TargetRingScale.x < ring4.transform.localScale.x) buttonPromptImage.enabled = true;
-            else if (ring5.transform.localScale.x < TargetRingScale.x && TargetRingScale.x < ring6.transform.localScale.x) buttonPromptImage.enabled = true;
+            if (ring1.transform.localScale.x < TargetRingScale.x && TargetRingScale.x < ring2.transform.localScale.x && !CompRing1) buttonPromptImage.enabled = true;
+            else if (ring3.transform.localScale.x < TargetRingScale.x && TargetRingScale.x < ring4.transform.localScale.x && !CompRing2) buttonPromptImage.enabled = true;
+            else if (ring5.transform.localScale.x < TargetRingScale.x && TargetRingScale.x < ring6.transform.localScale.x && !CompRing3) buttonPromptImage.enabled = true;
             else buttonPromptImage.enabled = false;
         }
 
-        if(isFlashing1)
-        {
-            StartCoroutine(FlashRedForOneSecond(targetRenderer1, isFlashing1));
-            isFlashing1 = false;
-            MinigameDone = true;
-        }
-        if (isFlashing2)
-        {
-            StartCoroutine(FlashRedForOneSecond(targetRenderer2, isFlashing2));
-            isFlashing2 = false;
-            MinigameDone = true;
-        }
-        if (isFlashing3)
-        {
-            StartCoroutine(FlashRedForOneSecond(targetRenderer3, isFlashing3));
-            isFlashing3 = false;
-            MinigameDone = true;
-        }
-
-        if ((ring2.transform.localScale.x < targetRing.transform.localScale.x && CompRing1 == false))
+        if ((ring2.transform.localScale.x < targetRing.transform.localScale.x && !CompRing1 && !isFlashing1))
         {
             isFlashing1 = true;
         }
-
-        if ((ring4.transform.localScale.x < targetRing.transform.localScale.x && CompRing2 == false))
+        else if ((ring4.transform.localScale.x < targetRing.transform.localScale.x && !CompRing2 && !isFlashing2))
         {
             isFlashing2 = true;
         }
-        if (ring6.transform.localScale.x < targetRing.transform.localScale.x && CompRing3 == false)
+        else if (ring6.transform.localScale.x < targetRing.transform.localScale.x && !CompRing3 && !isFlashing3)
         {
             isFlashing3 = true;
         }
 
+        if (isFlashing1)
+        {
+            isFlashing1 = false;
+            StartCoroutine(FlashRedForOneSecond(targetRenderer1.GetComponent<Animator>()));
+            MinigameDone = true;
+        }
+        else if (isFlashing2)
+        {
+            isFlashing2 = false;
+            StartCoroutine(FlashRedForOneSecond(targetRenderer2.GetComponent<Animator>()));
+            MinigameDone = true;
+        }
+        else if (isFlashing3)
+        {
+            isFlashing3 = false;
+            StartCoroutine(FlashRedForOneSecond(targetRenderer3.GetComponent<Animator>()));
+            MinigameDone = true;
+        }
     }
 
     private void SizeCheck(InputAction.CallbackContext Context)
@@ -188,7 +188,7 @@ public class CastMinigame : MinigameBase
         if(ring1.transform.localScale.x < targetRing.transform.localScale.x && targetRing.transform.localScale.x < ring2.transform.localScale.x)
         {
             CompRing1 = true;
-            targetRenderer1.color = Color.green;
+            targetRenderer1.GetComponent<Animator>().SetTrigger("Correct");
         }
 
         if ((ring2.transform.localScale.x < targetRing.transform.localScale.x && targetRing.transform.localScale.x < ring3.transform.localScale.x))
@@ -199,7 +199,7 @@ public class CastMinigame : MinigameBase
         if (ring3.transform.localScale.x < targetRing.transform.localScale.x && targetRing.transform.localScale.x < ring4.transform.localScale.x)
         {
             CompRing2 = true;
-            targetRenderer2.color = Color.green;
+            targetRenderer2.GetComponent<Animator>().SetTrigger("Correct");
         }
 
         if ((ring4.transform.localScale.x < targetRing.transform.localScale.x && targetRing.transform.localScale.x < ring5.transform.localScale.x))
@@ -211,7 +211,7 @@ public class CastMinigame : MinigameBase
         if (ring5.transform.localScale.x < targetRing.transform.localScale.x && targetRing.transform.localScale.x < ring6.transform.localScale.x)
         {
             CompRing3 = true;
-            targetRenderer3.color = Color.green;
+            targetRenderer3.GetComponent<Animator>().SetTrigger("Correct");
             MinigameDone = true;
             Win = true;
             active = false;
@@ -221,42 +221,32 @@ public class CastMinigame : MinigameBase
     }
 
     // Coroutine to flash the color red for the specified duration
-    private IEnumerator FlashRedForOneSecond(Image target, bool isFlashing)
-    {
-        float elapsedTime = 0f;
+    private IEnumerator FlashRedForOneSecond(Animator target)
+     {
+        if (target == null) yield break;
+        if (target.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f) yield break;
 
-        while (elapsedTime < flashDuration && isFlashing)
-        {
-            if (target != null)
-            {
-                // Change color to red
-                target.color = Color.red;
+        coroutinePlaying = true;
+        target.SetTrigger("Flash");
 
-                // Wait for the flash interval
-                yield return new WaitForSeconds(flashInterval);
+        yield return 0;
+        while (target.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f) yield return null;
 
-                // Change back to original color
-                target.color = originalColor;
-
-                // Wait for the same interval before switching again
-                yield return new WaitForSeconds(flashInterval);
-
-                // Update the elapsed time (each flash cycle is 2 * flashInterval)
-                elapsedTime += 2 * flashInterval;
-            }
-        }
-        isFlashing = false;
-        target.color = originalColor;
         targetRing.transform.localScale = Vector3.zero;
         targetRenderer1.color = originalColor;
         targetRenderer2.color = originalColor;
         targetRenderer3.color = originalColor;
+        targetRenderer1.GetComponent<Animator>().SetTrigger("Reset");
+        targetRenderer2.GetComponent<Animator>().SetTrigger("Reset");
+        targetRenderer3.GetComponent<Animator>().SetTrigger("Reset");
         CompRing1 = false;
         CompRing2 = false;
         CompRing3 = false;
         MinigameDone = false;
 
-        yield return null;
+        coroutinePlaying = false;
+
+        yield break;
 
        
     }
