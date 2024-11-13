b08791273e41fef809d75d74a56a3f8d971a6e35
cjoyner150 <cjoyner150@gmail.com>
2024-11-13 13:45:50 -0500

    FishTimeline script + prefab

--- Assets/Scripts/FishTimeline.cs
+++ Assets/Scripts/FishTimeline.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class FishTimeline : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] PlayableDirector director;
+    [SerializeField] float minTime;
+    [SerializeField] float maxTime;
+
     void Start()
     {
-        
+        director.stopped += delegate { StartCoroutine(PlayTimeline()); };
+        StartCoroutine(PlayTimeline());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator PlayTimeline()
     {
-        
+
+        yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+
+        director.Play();
+
     }
 }
