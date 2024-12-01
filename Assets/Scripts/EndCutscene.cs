183838b5e471f2621ffe51bf96361771a8aece2d
PoyoMain <CLC9512@GMAIL.COM>
2024-11-30 21:28:50 -0500

    End cutscene is more dynamic with caught fish

---
+++ Assets/Scripts/EndCutscene.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndCutscene : MonoBehaviour
+{
+    [Header("Inspector Object")]
+    [SerializeField] private Transform spawnTransform;
+
+    [Header("Listen Events")]
+    [SerializeField] private FishEventChannelSO fishCaught;
+
+    private Fish fish;
+
+    private void OnEnable()
+    {
+        fishCaught.OnEventRaised += SetFish;
+    }
+
+    private void OnDisable()
+    {
+        fishCaught.OnEventRaised -= SetFish;
+    }
+
+    private void SetFish(Fish fishCaught)
+    {
+        fish = fishCaught;
+    }
+
+    public void SpawnFish()
+    {
+        GameObject fishObject = Instantiate(fish.Model, Vector3.zero, Quaternion.identity, spawnTransform);
+        fishObject.transform.localPosition = Vector3.zero;
+    }
+}
